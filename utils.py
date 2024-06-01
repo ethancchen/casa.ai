f26e3fbd55950531c7aca8e9f35153edb88f866f
Mehul-Gandhi <mgandhi1127@gmail.com>
2024-06-01 15:17:56 -0700

    Add script for video

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
+from octoai.client import OctoAI
+from dotenv import load_dotenv
+import os
+import base64
 
+load_dotenv()
 
 # function to load the vectordatabase
 def load_knowledgeBase():
@@ -34,3 +39,41 @@ def load_prompt():
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
+
+
+def generate_video():
+    with open("v2.png", "rb") as picture:
+        picture_data = picture.read()
+        
+        # Encode the file content to base64
+        base64_string = base64.b64encode(picture_data)
+        
+        # Convert the bytes to string
+        base64_string = base64_string.decode('utf-8')
+        
+    client = OctoAI(
+        api_key=os.environ["OCTOAI_API_TOKEN"],
+    )
+    response = client.image_gen.generate_svd(
+        image=base64_string,
+        cfg_scale=3,
+        steps=25,
+        motion_scale=0.5,
+        noise_aug_strength=0.02,
+        num_videos=1,
+        fps=7
+    )
+    #import pdb; pdb.set_trace()
+    print(response.videos)
+    video_base64 = response.videos[0].video # Adjust this based on the actual response structure
+
+    # Decode the base64 video data
+    video_data = base64.b64decode(video_base64)
+
+    # Save the video data to a file
+    with open('generated_video.mp4', 'wb') as video_file:
+        video_file.write(video_data)
+
+    print("Video saved as 'generated_video.mp4'")
+
